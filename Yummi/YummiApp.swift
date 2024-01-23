4981e3370f879873f860c89d600b82a38ef23878
freddiehoward <howard.f@etoncollege.org.uk>
2024-01-23 12:53:03 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Howard, Freddie (ABH) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
