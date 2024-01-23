cc4be0f57a1b3906aaf26ac8f4c82e50d6ad1be1
freddiehoward <howard.f@etoncollege.org.uk>
2024-01-23 13:18:15 +0000

    forgot to stage mb

--- Yummi/Ingredients.swift
+++ Yummi/Ingredients.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct Ingredients {
+    
+    private var name: String
+    
+    private var quantity: Int
+    
+    private var unit: String
+    
+    private var category: String
+    
+    private var expiryDate: Int
+    
+    
+    func displayStats() -> String {
+        
+        return """
+name: \(self.name)
+quantity: \(self.quantity)
+unit: \(self.unit)
+category: \(self.category)
+expiryDate: \(self.expiryDate)
+"""
+        
+        
+    }
+    
+}
